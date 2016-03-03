1e8cff2629ba53cb2c0103383c8947ba71f3ea00
levinebe <levine.be@husky.neu.edu>
2016-03-03 16:24:06 -0500

    Basic UI layout. Panels for health/gold/wave. Two special power buttons.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public int healthCount;
+	public int goldCount;
+	public int currentWave;
+	public int maxWave;
+
+	public Text healthText;
+	public Text goldText;
+	public Text waveText;
+	
+	void Start () {
+		healthCount = 0;
+		goldCount = 0;
+		currentWave = 1;
+		maxWave = 10;
+		UpdateHealthText ();
+		UpdateGoldText ();
+		UpdateWaveText ();
+	}
+
+	void Update () {
+		UpdateHealthText ();
+		UpdateGoldText ();
+		UpdateWaveText ();
+	}
+
+	void UpdateHealthText ()
+	{
+		healthText.text = healthCount.ToString ();
+	}
+
+	void UpdateGoldText ()
+	{
+		goldText.text = goldCount.ToString ();
+	}
+
+	void UpdateWaveText ()
+	{
+		waveText.text = "WAVE   " + currentWave.ToString () + " / " + maxWave.ToString ();
+	}
+
+}
