578514668150202f88490594429937074f6d6c53
Sean Goodwin <Sean.e.goodwin@gmail.com>
2016-03-02 23:54:34 -0500

    Initial implementation of towers

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+    public float bulletSpeed = 1;
+    public float bulletDamage = 1;
+    private TowerScript parentTower;
+    private Transform targetTranform;
+    private string enemyTag = "Enemy";
+
+    public void Setup(TowerScript parent, Transform target, float speed, float damage) 
+    {
+        parentTower = parent;
+        targetTranform = target;
+        bulletSpeed = speed;
+        bulletDamage = damage;
+    }
+	
+	void Update () 
+    {
+	    if (targetTranform != null) 
+        {
+	        transform.position = Vector3.MoveTowards(transform.position, targetTranform.position, bulletSpeed * Time.deltaTime);
+	    }
+	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider) 
+    {
+        if (otherCollider.gameObject.tag == enemyTag) 
+        {
+            parentTower.RemoveDeadBullet(transform);
+            Destroy(gameObject); //TODO implement some sort of method for all moral units to take damage
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/TowerScript.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class TowerScript : MonoBehaviour {
+
+    public Transform bulletPrefab;
+    public float enemyDetectionRadius = 1f;
+    public int maxBulletCount = 5;
+    private const string enemyTag = "Enemy";
+    private CircleCollider2D circleCollider2D;
+    private List<Transform> enemiesInRange;
+    private List<Transform> bullets;
+
+	void Start () 
+    {
+        enemiesInRange = new List<Transform>();
+        bullets = new List<Transform>();
+        circleCollider2D = GetComponent<CircleCollider2D>();
+	    circleCollider2D.radius = enemyDetectionRadius;
+	}
+
+    void Setup(float detectionRadius, int bulletCount) 
+    {
+        enemyDetectionRadius = detectionRadius;
+        circleCollider2D.radius = enemyDetectionRadius;
+        maxBulletCount = bulletCount;
+    }
+
+	void Update () 
+    {
+	    enemiesInRange = enemiesInRange.OrderBy(go => Vector3.Distance(go.transform.position, transform.position)).ToList();
+	    var enemyToAtack = enemiesInRange.FirstOrDefault();
+	    if (enemyToAtack != null) 
+        {
+	        AttackEnemy(enemyToAtack);
+	    }
+	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider) 
+    {
+        if (otherCollider.gameObject.tag == enemyTag) 
+        {
+            if (!enemiesInRange.Contains(otherCollider.gameObject.transform)) 
+            {
+                enemiesInRange.Add(otherCollider.gameObject.transform);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D otherCollider) 
+    {
+        enemiesInRange.Remove(otherCollider.transform);
+    }
+
+    void AttackEnemy(Transform enemyTransform) 
+    {
+        if (bullets.Count < maxBulletCount) 
+        {
+            var bullet = Instantiate(bulletPrefab, Vector3.zero, Quaternion.identity) as Transform;
+            if (bullet != null) 
+            {
+                bullets.Add(bullet);
+                bullet.GetComponent<BulletScript>().Setup(this, enemyTransform, 5, 1);
+            }
+        }
+    }
+
+    public void RemoveDeadBullet(Transform bullet) 
+    {
+        bullets.Remove(bullet);
+    }
+
+}
